bc734cdff4ea2d2358bfc4b6561164c81949c57a
Azarius <as.haerens@gmail.com>
2021-02-26 13:16:06 +0100

    add rebase countdown preview

--- src/domains/Dapp/views/Rebase/rebase.component.js
+++ src/domains/Dapp/views/Rebase/rebase.component.js
@@ -1,7 +1,8 @@
 import { Fragment } from 'react';
 import { useWeb3React } from '@web3-react/core';
 
-import { Section, DisconnectedWalletCard } from '@dapp/components';
+import { Countdown } from '@core/components';
+import { Section, DisconnectedWalletCard, LabeledCard } from '@dapp/components';
 
 import RebaseVariables from './components/rebase-variables.component';
 import { StyledRebase, StyledGridItem } from './rebase.styles';
@@ -17,6 +18,14 @@ const Rebase = ()  => {
 
     return (
         <Fragment>
+			<Section label="rebase time">
+				<LabeledCard
+					label="time remaining"
+					gutter={40}
+				>
+					<Countdown endDate="2021-04-03" />
+				</LabeledCard>
+			</Section>
 			<Section
 				label="Variables"
 				info="**update** info about rebase variables"
